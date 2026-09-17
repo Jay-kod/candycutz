bcb9947a9701144fb14a9268eb1a64b7744a0164
Jay-kod <james1ofourtime@gmail.com>
2026-09-17 15:58:55 +0100

    chore: remove generated phase 0 artifacts

--- start-barber-app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title CandyCutz Unified Mobile App (Barber Entry)
-echo [NOTE] The customer app and barber app have converged into ONE unified app: candycutz-mobile-app
-call "%~dp0start-mobile-app.bat"
